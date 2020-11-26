5d6e81b44f52e04b1267dfbf71e19b68902f8935
erik1500 <erik1500@stud.kea.dk>
2020-11-26 10:47:58 +0100

    Initial commit

---
+++ src/test/java/com/nerds4hire/ships/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nerds4hire.ships.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
